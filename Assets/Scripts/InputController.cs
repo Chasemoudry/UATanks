9df54c52a8f9b052acd952b05840f2327a54c490
chasemoudry <chasemoudry@gmail.com>
2018-02-20 17:43:30 -0700

    New: Cinemachine support New: Low-poly assets New: VehicleData scriptable object New: Basic player controller (with prefab)

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    private VehicleData data;
+
+    private delegate void FireButton();
+
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        this.controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        this.StartCoroutine(MovementInput());
+    }
+
+    private void OnDisable()
+    {
+        this.StopAllCoroutines();
+    }
+
+    private IEnumerator MovementInput()
+    {
+        while (true)
+        {
+            float horzMove = Input.GetAxis("Horizontal");
+            float vertMove = Input.GetAxis("Vertical");
+
+            if (vertMove > 0)
+            {
+                horzMove *= this.data.ForwardSpeed * Time.deltaTime;
+                vertMove *= this.data.ForwardSpeed * Time.deltaTime;
+            }
+            else
+            {
+                horzMove *= this.data.ReverseSpeed * Time.deltaTime;
+                vertMove *= this.data.ReverseSpeed * Time.deltaTime;
+            }
+
+            this.controller.SimpleMove(new Vector3(horzMove, 0, vertMove));
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/VehicleData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data Objects/Vehicle Data")]
+public class VehicleData : ScriptableObject
+{
+    public float ForwardSpeed { get { return this.forwardSpeed; } private set { this.forwardSpeed = value; } }
+
+    public float ReverseSpeed { get { return this.reverseSpeed; } private set { this.reverseSpeed = value; } }
+
+    public int RotateSpeed { get { return this.rotateSpeed; } private set { this.rotateSpeed = value; } }
+
+    public float ProjectileForce { get { return this.projectileForce; } private set { this.projectileForce = value; } }
+
+    public float ProjectileDamage { get { return this.projectileDamage; } private set { this.projectileDamage = value; } }
+
+    public float TimeBetweenShots { get { return this.timeBetweenShots; } private set { this.timeBetweenShots = value; } }
+
+    public int MaxHealth { get { return this.maxHealth; } set { this.maxHealth = value; } }
+
+    public int VehicleWorth { get { return this.vehicleWorth; } set { this.vehicleWorth = value; } }
+
+    [Header("Movement Settings")]
+    [SerializeField, Range(1, 20)]
+    private float forwardSpeed = 10;
+    [SerializeField, Range(1, 20)]
+    private float reverseSpeed = 5;
+    [SerializeField, Range(5, 90)]
+    private int rotateSpeed = 20;
+
+    [Header("Projectile Settings")]
+    [SerializeField, Range(1, 20)]
+    private float projectileForce = 10;
+    [SerializeField, Range(0, 20)]
+    private float projectileDamage = 10;
+    [SerializeField, Range(0, 10)]
+    private float timeBetweenShots = 1;
+
+    [Header("Durability Settings")]
+    [SerializeField, Range(10, 200)]
+    private int maxHealth = 100;
+
+    [Header("Score Settings")]
+    [Tooltip("The amount of points this vehicle awards on destruction.")]
+    [SerializeField, Range(0, 200)]
+    private int vehicleWorth = 0;
+}
